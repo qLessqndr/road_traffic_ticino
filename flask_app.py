fc35b7065d0c5718b4cc13a4a836c0dc8573b9a6
qLessqndr <alecarnio21@gmail.com>
2024-10-01 11:38:37 +0200

    Add files via upload

--- flask_app.py
+++ flask_app.py
@@ -181,4 +181,4 @@ def update_traffic_data():
 
 if __name__ == '__main__':
     webbrowser.open("http://127.0.0.1:5000/")
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
